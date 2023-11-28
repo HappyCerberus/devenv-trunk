81c50efb9d09889c04daf34e40ad9566a0756721
RNDr. Simon Toth <happy.cerberus@gmail.com>
2023-11-28 11:18:13 +0100

    Add Ninja build (for C++20 modules).

---
+++ retag.bat
@@ -0,0 +1,11 @@
+set TAG=20231128
+
+docker manifest create happycerberus/devenv-trunk:latest --amend happycerberus/devenv-trunk:latest-arm64 --amend happycerberus/devenv-trunk:latest-amd64
+docker manifest annotate happycerberus/devenv-trunk:latest happycerberus/devenv-trunk:latest-amd64 --arch amd64
+docker manifest annotate happycerberus/devenv-trunk:latest happycerberus/devenv-trunk:latest-arm64 --arch arm64
+docker manifest push happycerberus/devenv-trunk:latest
+
+docker manifest create happycerberus/devenv-trunk:%TAG% --amend happycerberus/devenv-trunk:%TAG%-arm64 --amend happycerberus/devenv-trunk:%TAG%-amd64
+docker manifest annotate happycerberus/devenv-trunk:%TAG% happycerberus/devenv-trunk:%TAG%-amd64 --arch amd64
+docker manifest annotate happycerberus/devenv-trunk:%TAG% happycerberus/devenv-trunk:%TAG%-arm64 --arch arm64
+docker manifest push happycerberus/devenv-trunk:%TAG%
